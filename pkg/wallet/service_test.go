2a3168cd37f0b042403b1786ef2ae05002f86607
Nekruz <nekruzihikmatullo@gmail.com>
2021-12-26 13:27:19 +0300

    добавлен метод Reject и тесты к нему

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -1,9 +1,11 @@
 package wallet
 
 import (
-	"testing"
 	"fmt"
 	"reflect"
+	"testing"
+
+	"github.com/nekruz08/wallet/pkg/types"
 )
 
 func TestService_FindAccountByID_success(t *testing.T) {
@@ -28,6 +30,8 @@ func TestService_FindAccountByID_success(t *testing.T) {
 	}
 }
 
+//-----------------------------------------------
+
 func TestService_FindAccountByID_notExist(t *testing.T) {
 	svc := &Service{}
 	account:=int64(2)
@@ -42,5 +46,125 @@ func TestService_FindAccountByID_notExist(t *testing.T) {
 
 	if reflect.DeepEqual(account,account1){
 		t.Errorf("invalid result, expected: %v, actual: %v",account,account1)
+	}
+}
+
+//-----------------------------------------------
+
+func TestSevice_Reject_success(t *testing.T) {
+	svc := &Service{}
+	account,err:=svc.RegisterAccount("+992888844290")
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	err=svc.Deposit(account.ID,10)
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	payment,err:=svc.Pay(account.ID,5,"sadaqa")
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	err=svc.Reject(payment.ID)
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	acc,err:=svc.FindAccountByID(payment.AccountID)
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	if !reflect.DeepEqual(account,acc){
+		t.Errorf("invalid result, expected: %v, actual: %v",account,acc)
+	}
+}
+
+//-----------------------------------------------
+func TestSevice_Reject_notFound(t *testing.T) {
+	svc := &Service{}
+	account,err:=svc.RegisterAccount("+992888844290")
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	err=svc.Deposit(account.ID,10)
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	payment,err:=svc.Pay(account.ID,5,"sadaqa")
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+	
+	wrongPayment:=&types.Payment{
+		ID: "helloWorld",
+	}
+	err=svc.Reject(wrongPayment.ID)
+	if err!=nil{
+		switch err{
+		case ErrPaymentNotFound:
+			fmt.Println("ErrPaymentNotFound")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	acc,err:=svc.FindAccountByID(payment.AccountID)
+	if err!=nil{
+		switch err{
+		case ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователья не найден")	
+		}
+		return
+	}
+
+	if reflect.DeepEqual(wrongPayment,acc){
+		t.Errorf("invalid result, expected: %v, actual: %v",wrongPayment,acc)
 	}
 }
